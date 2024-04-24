93b8d51c6c675bc969ad7c6f859f4b3ef075679d
Wael El Hachimi <wael@unik.ma>
2024-04-24 21:27:08 +0100

    first comit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+vim.g.mapleader = ","
+
+-- Fast saving
+keymap.set("n", "<leader>w", ":w!<cr>")
+
+-- Remaping the esc button
+keymap.set("i", "jk", "<Esc>")
